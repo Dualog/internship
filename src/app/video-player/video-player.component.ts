a74fc2f4c4c9cd1088abc8506fce67500faeb9d1
stellander <kingstellander@gmail.com>
2022-01-07 12:05:47 +0100

    Update readme and add intern photo

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -3,23 +3,26 @@ import { Component, OnInit } from '@angular/core';
 let apiLoaded = false;
 
 @Component({
-  template: '<youtube-player videoId="anpG7GsRtVg" [width]="currentScreenWidth" [height]="currentScreenheight"></youtube-player>',
+  template:
+    '<youtube-player videoId="anpG7GsRtVg" [width]="currentScreenWidth" [height]="currentScreenheight"></youtube-player>',
   selector: 'video-player',
 })
 export class VideoPlayerComponent implements OnInit {
   currentScreenWidth = 0;
-  currentScreenheight= 0;
-
+  currentScreenheight = 0;
 
   ngOnInit() {
     const lm = document.getElementsByClassName('dp-container');
-    const test = (+getComputedStyle(lm[1]).paddingRight.split('px')[0]) * 2;
-    const sw = ((lm[1] as HTMLElement).offsetWidth - test - 16) / 2;
-    this.currentScreenWidth = window.innerWidth > 600 ? (sw  )  : window.innerWidth - 32;
-    if(this.currentScreenWidth > 800) 
-      this.currentScreenWidth = 800;
-    
-      this.currentScreenheight = this.currentScreenWidth / 1.64;
+    const paddingRight = +getComputedStyle(lm[1]).paddingRight.split('px')[0] * 2;
+    const offsetWidth = (lm[1] as HTMLElement).offsetWidth;
+    if (window.innerWidth > 800) {
+      this.currentScreenWidth = (offsetWidth - paddingRight - 16) / 2;
+      if (this.currentScreenWidth > 800) this.currentScreenWidth = 800;
+    } else {
+      this.currentScreenWidth = offsetWidth - paddingRight;
+    }
+
+    this.currentScreenheight = this.currentScreenWidth / 1.64;
     if (!apiLoaded) {
       // This code loads the IFrame Player API code asynchronously, according to the instructions at
       // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
@@ -29,5 +32,4 @@ export class VideoPlayerComponent implements OnInit {
       apiLoaded = true;
     }
   }
-
 }
